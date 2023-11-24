5831af00d2c6ebe858899d3cf385923bbe9d4200
Sudip Mandal <skmxyt@gmail.com>
2023-11-25 03:23:32 +0530

    authMiddleware

--- src/controllers/auth.ts
+++ src/controllers/auth.ts
@@ -1,53 +1,63 @@
-
-import asyncHandler from "express-async-handler"
-import {getClient}  from "../config/prismadb";
-import bcrypt from 'bcrypt'
-import { generateToken,verifyToken } from "../libs/jwtToken";
+import { Request, Response } from 'express';
+import asyncHandler from 'express-async-handler';
+import { getClient } from '../config/prismadb';
+import bcrypt from 'bcrypt';
+import { generateToken, verifyToken } from '../libs/jwtToken';
 
 enum Role {
-    STUDENT = "STUDENT",
-    FACULTY = "FACULTY",
+  STUDENT = 'STUDENT',
+  FACULTY = 'FACULTY',
 }
 
-const prisma=getClient();
+interface RegisterUserRequest extends Request {
+  body: {
+    name: string;
+    rollno?: string;
+    email?: string;
+    role?: Role;
+    password: string;
+  };
+}
 
-export const registerUser = asyncHandler(async (req, res) => {
-  
-    const { name,rollno,password,role,email } = (req.body) as {
-        name:string,
-        rollno?:string
-        email?:string
-        role?:Role
-        password:string
-    };
-  
+export const registerUser = asyncHandler(
+  async (req: RegisterUserRequest, res: Response): Promise<void> => {
+    const { name, rollno, password, role, email } = req.body;
+
     const hashedPassword = await bcrypt.hash(password, 12);
 
-
-    const user = await prisma.user.create(
-        {
-            data:{
-                name,
-                rollno,
-                password:hashedPassword,
-                email,
-                role
-            }
-        }
-    )
+    const prisma = getClient();
+    const user = await prisma.user.create({
+      data: {
+        name,
+        rollno,
+        password: hashedPassword,
+        email,
+        role,
+      },
+    });
 
     let token;
 
-    if(user){
-        token= await generateToken(user.id);
+    if (user) {
+      token = await generateToken(user.id);
     }
 
-    
-    return res.json({...user,token}).status(200)
-  });
-  
+    res.json({ ...user, token }).status(200);
+  }
+);
 
-export const getuserDetails = asyncHandler(async (req, res) => {
-  
-    return res.json({user:req?.user}).status(200)
-  });
+interface GetuserDetailsrRequest extends Request {
+    user: {
+      name: string;
+      rollno?: string;
+      email?: string;
+      role?: Role;
+      password: string;
+    };
+  }
+
+export const getuserDetails = asyncHandler(
+  async (req: GetuserDetailsrRequest, res: Response): Promise<void> => {
+    res.json({ user: req?.user }).status(200);
+  }
+);
--- src/middleware/authMiddleware.ts
+++ src/middleware/authMiddleware.ts
@@ -1,34 +1,45 @@
-import asyncHandler from "express-async-handler";
-import { getClient } from "../config/prismadb";
-import { generateToken, verifyToken } from "../libs/jwtToken";
-import ErrorHandler from "../libs/ErrorHandler";
+import { Request, Response, NextFunction } from 'express';
+import asyncHandler from 'express-async-handler';
+import { getClient } from '../config/prismadb';
+import { generateToken, verifyToken } from '../libs/jwtToken';
+import ErrorHandler from '../libs/ErrorHandler';
 
 const prisma = getClient();
 
-export const isAuthenticated = asyncHandler(async (req, res, next):Promise<void> => {
-  let token;
+interface DecodedData {
+  id: string;
+  // Add other properties from your decoded token if needed
+}
 
-  // Check if the Authorization header is present and starts with 'Bearer'
-  if (
-    req.headers.authorization &&
-    req.headers.authorization.startsWith("Bearer")
-  ) {
-    // Get token from the Authorization header
-    token = req.headers.authorization.split(" ")[1];
+// Extend the Request interface to include the user property
+interface AuthenticatedRequest extends Request {
+  user?: string; // Replace YourUserType with the actual type of your user
+}
+
+export const isAuthenticated = asyncHandler(
+  async (req: AuthenticatedRequest, res: Response, next: NextFunction): Promise<void> => {
+    let token;
+
+    // Check if the Authorization header is present and starts with 'Bearer'
+    if (req.headers.authorization && req.headers.authorization.startsWith('Bearer')) {
+      // Get token from the Authorization header
+      token = req.headers.authorization.split(' ')[1];
+    }
+
+    if (!token) {
+      return next(new ErrorHandler('Please login to access this resource', 401));
+    }
+
+    // Verify the token
+    const decodedData = await verifyToken(token) as DecodedData;
+
+    // Find the user associated with the decoded data
+    req.user = await prisma.user.findUnique({
+      where: {
+        id: decodedData.id,
+      },
+    });
+
+    next();
   }
-
-  if (!token) {
-    return next(new ErrorHandler("Please login to access this resource", 401));
-  }
-
-  // Verify the token
-  const decodedData = await verifyToken(token);
-  // Find the user associated with the decoded data
-  req.user = await prisma.user.findUnique({
-    where: {
-      id: decodedData.id as string,
-    },
-  });
-
-  next();
-});
+);
